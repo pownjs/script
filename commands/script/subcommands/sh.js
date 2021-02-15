b28eb53b4594f829d2e32d6c1448eeb7b6a3c889
Petko D. Petkov <pdp@websecurify.com>
2021-02-15 22:36:49 +0000

    Improved shell execution.

--- commands/script/subcommands/sh.js
+++ commands/script/subcommands/sh.js
@@ -1,20 +1,26 @@
 exports.yargs = {
-    command: 'sh',
+    command: 'sh [cmd...]',
     describe: 'Run shell command',
 
     builder: {
         command: {
             describe: 'The shell command',
             type: 'string',
-            alias: 'c'
+            alias: ['c']
         }
     },
 
     handler: async(argv) => {
-        const { command } = argv
+        const { command, cmd } = argv
 
         const { execSync } = require('child_process')
 
-        execSync(command, { stdio: 'inherit' })
+        if (command) {
+            execSync(command, { stdio: 'inherit' })
+        }
+        else
+        if (cmd) {
+            execSync(cmd.join(' '), { stdio: 'inherit' })
+        }
     }
 }
